bbec1b08d0fff29b1a674c5fa58015dc518b8dfe
Nina Luo <30792189+Ni4991@users.noreply.github.com>
2018-07-07 11:10:54 -0700

    update

--- logic/Exchange.ts
+++ logic/Exchange.ts
@@ -3,13 +3,12 @@ import { Player } from "./Player";
 
 /**
  * the class Merchandise
- * this class will be used in the Exchange to stand for a good
+ * this class will be used in the Exchange to reperesent a material
  * will also be used in the store in the TechBoard class
  * @argument give: the material used to trade
  * @argument get: the material you want
  * @argument numGive: the quantity to give in one trade
  * @argument numGet: the quantity you get in one trade
- * @author nina;
  */
 class Merchandise{
   public give: Material;
@@ -28,45 +27,44 @@ class Merchandise{
 /**
  * Exchange has nine kinds of Merchandise;
  * this class will be initialized in the Action class;
- * after initialized, nine merchandises will be push into an array;
- * @author nina;
+ * after initialized, nine merchandises will be pushed into an array;
  */
 class Exchange{
-  public powerQIC: Merchandise;
-  public powerOre: Merchandise;
-  public powerSicence: Merchandise;
-  public powerGold: Merchandise;
-  public oreGold: Merchandise;
-  public oreExtra: Merchandise;
-  public qicRange: Merchandise;
-  public qicOre: Merchandise;
-  public scienceGold: Merchandise;
+  public powerToQIC: Merchandise;
+  public powerToOre: Merchandise;
+  public powerToSicence: Merchandise;
+  public powerToGold: Merchandise;
+  public oreToGold: Merchandise;
+  public oreToExtra: Merchandise;
+  public qicToRange: Merchandise;
+  public qicToOre: Merchandise;
+  public scienceToGold: Merchandise;
   public exchanges: Merchandise[];
 
   constructor(){
-    this.powerQIC = new Merchandise(Material.Charge, Material.QIC, 4, 1);
-    this.powerOre = new Merchandise(Material.Charge, Material.Ore, 3, 1);
-    this.powerSicence = new Merchandise(Material.Charge, Material.Science, 4, 1);
-    this.powerGold = new Merchandise(Material.Charge, Material.Gold, 1, 1);
-    this.oreGold = new Merchandise(Material.Ore, Material.Gold, 1, 1);
-    this.oreExtra = new Merchandise(Material.Ore, Material.Power, 1, 1);
-    this.qicRange = new Merchandise(Material.QIC, Material.SpecialRange, 1, 2);
-    this.qicOre = new Merchandise(Material.QIC, Material.Ore, 1, 1);
-    this.scienceGold = new Merchandise(Material.Science, Material.Gold, 1, 1);
-    this.exchanges.push(this.powerQIC);
-    this.exchanges.push(this.powerOre);
-    this.exchanges.push(this.powerSicence);
-    this.exchanges.push(this.powerGold);
-    this.exchanges.push(this.oreGold);
-    this.exchanges.push(this.oreExtra);
-    this.exchanges.push(this.qicRange);
-    this.exchanges.push(this.qicOre);
-    this.exchanges.push(this.scienceGold);
+    this.powerToQIC = new Merchandise(Material.Charge, Material.QIC, 4, 1);
+    this.powerToOre = new Merchandise(Material.Charge, Material.Ore, 3, 1);
+    this.powerToSicence = new Merchandise(Material.Charge, Material.Science, 4, 1);
+    this.powerToGold = new Merchandise(Material.Charge, Material.Gold, 1, 1);
+    this.oreToGold = new Merchandise(Material.Ore, Material.Gold, 1, 1);
+    this.oreToExtra = new Merchandise(Material.Ore, Material.Power, 1, 1);
+    this.qicToRange = new Merchandise(Material.QIC, Material.SpecialRange, 1, 2);
+    this.qicToOre = new Merchandise(Material.QIC, Material.Ore, 1, 1);
+    this.scienceToGold = new Merchandise(Material.Science, Material.Gold, 1, 1);
+    this.exchanges.push(this.powerToQIC);
+    this.exchanges.push(this.powerToOre);
+    this.exchanges.push(this.powerToSicence);
+    this.exchanges.push(this.powerToGold);
+    this.exchanges.push(this.oreToGold);
+    this.exchanges.push(this.oreToExtra);
+    this.exchanges.push(this.qicToRange);
+    this.exchanges.push(this.qicToOre);
+    this.exchanges.push(this.scienceToGold);
   }
 
   /**
-   * the function to make several times of trade;
-   * @param player the player who do the trade;
+   * the function to make a particular times of trade;
+   * @param player the player who does the trade;
    * @param give the material given out;
    * @param get the material get;
    * @param times the times of trade
@@ -74,7 +72,7 @@ class Exchange{
   public trade(player: Player, give: Material, get: Material, times: number){
     var good = null;
     // find the type of trading
-    for(var i = 0; i < this.exchanges.length; i++){
+    for(let i = 0; i < this.exchanges.length; i++){
       if(this.exchanges[i].give === give && this.exchanges[i].get === get){
         good = this.exchanges[i];
         break;
@@ -99,7 +97,11 @@ class Exchange{
     if(give === Material.QIC) player.qic -= totalGive;
     if(give === Material.Science) player.science -= totalGive;
 
-    if(get === Material.SpecialRange) player.range += totalGet;// problem: how to reduce it after one action?
+    /**
+     * // problem: how to reduce it after one action?
+     * In player's class, if does exchange and get === Material.SpecialRange, do player.range-=totalGet after the exchange
+     */
+    if(get === Material.SpecialRange) player.range += totalGet;
     if(get === Material.QIC) player.qic += totalGet;
     if(get === Material.Ore) player.ore += totalGet;
     if(get === Material.Gold) player.gold += totalGet;
@@ -112,7 +114,6 @@ class Exchange{
    * @param player 
    * @param give the material given out
    * @param quantity the total number of resource needed
-   * @author nina
    */
   public checkResources(player: Player, give: Material, quantity: number){
     if(give === Material.Charge) return player.power3 >= quantity;
